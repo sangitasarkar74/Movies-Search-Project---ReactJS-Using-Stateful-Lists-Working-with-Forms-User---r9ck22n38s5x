cbbba71235faa22a7534bdf7287ee11561510cb3
sangitasarkar74 <sangitasarkar@gmail.com>
2022-12-15 00:20:54 +0530

    first change

--- src/components/App.js
+++ src/components/App.js
@@ -6,27 +6,33 @@ const App = () => {
     const [inputValue, setInputValue] = useState("");
     const [results, setResult] = useState(null);
 
-    function handleClick(e) {}
+    function handleClick(e) {
+        const movieList = movies.filter(movie => movie.title.includes(inputValue)).map(movie => movie.title);
+        setResult(movieList)
+    }
 
-    function handleChange(e) {}
+    function handleChange(e) {
+        setInputValue(e.target.value);
+        e.preventDefault();
+    }
 
     return (
         <div id="main">
-            <form id="form">
+            <form id="form" onSubmit={handleChange}>
                 <input
                     type="text"
                     onChange={handleChange}
                     value={inputValue}
                     id="movie-input"
                 ></input>
-                <button id="button" onClick={handleClick}>
+                <button id="button" type="button" onClick={handleClick}>
                     Search
                 </button>
             </form>
             <div id="result">
                 {results &&
                     results.map((movie) => (
-                        <div className="movie">{movie}</div>
+                        <div className="movie" key={movie.id}>{movie.title}</div>
                     ))}
             </div>
         </div>
